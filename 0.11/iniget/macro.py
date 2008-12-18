cfa40c392da781f7fd745783718217d981fde3e7
Douglas Clifton <dwclifton@gmail.com>
2008-12-18 02:25:07 -0500

    Latest revision, still borked in a wiki heading context

--- 0.11/iniget/macro.py
+++ 0.11/iniget/macro.py
@@ -1,41 +1,39 @@
-"""@package IniGetMacro
-   @file macro.py
-   @brief The IniGetMacro class
+""" @package IniGetMacro
+    @file macro.py
+    @brief The IniGetMacro class
 
-   Return the value of a trac.ini [section] option.
+    Return the value of a trac.ini [section] option.
 
-   @author Douglas Clifton <dwclifton@gmail.com>
-   @date December, 2008
-   @version 0.11.1
+    @author Douglas Clifton <dwclifton@gmail.com>
+    @date December, 2008
+    @version 0.11.1
 """
 
-#from trac.core import *
+from re import *
+from trac.core import *
+from StringIO import StringIO
+from trac.wiki import Formatter
+from trac.util.html import Markup
 from trac.wiki.macros import WikiMacroBase
 from trac.wiki.formatter import system_message
-#from trac.wiki.model import WikiPage
 from trac.config import Option
-import re
 
 class IniGetMacro(WikiMacroBase):
 
-    def expand_macro(self, formatter, name, param):
+    def expand_macro(self, formatter, name, args):
         """return value of a trac.ini [section].option"""
         
-        if param.find('.') == -1:
-            return system_message('%s: Invalid parameter: "%s"'%(name, param))
-        section, option = self.parse(param)
+        if args.find('.') == -1:
+            return system_message('%s: Invalid parameter: "%s"'%(name, args))
+
+        section, option = re.sub('\s+', '', args).split('.', 1)
+
         if self.config.has_option(section, option):
-            return self.config.get(section, option)
+            out = StringIO()
+            text = self.config.get(section, option)
+            Formatter(self.env, formatter.context).format(text, out)
+            return Markup(out.getvalue())
         else:
             return system_message('%s: No option "%s" in section [%s]'%(name,
                                                                         option,
                                                                         section))
-    def parse(self, param):
-        """return (section, option) tuple from parameter string"""
-        
-        try:
-            section, option = re.sub('\s+', '', param).split('.', 1)
-        except ValueError:
-            return
-        return (section, option)
-
