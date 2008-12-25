89adb62b3658373b7c9ab268964cb448438eea6f
Douglas Clifton <dwclifton@gmail.com>
2008-12-25 01:05:50 -0500

    Simplified fetching project name in welcome/macro.py

--- 0.11/welcome/macro.py
+++ 0.11/welcome/macro.py
@@ -13,7 +13,6 @@ from trac.core import *
 from trac.wiki.macros import WikiMacroBase
 from trac.wiki.formatter import system_message
 from trac.wiki.api import parse_args
-from trac.config import Option
 from genshi.builder import tag
 import re
 
@@ -22,11 +21,11 @@ __all__ = ['WelcomeMacro']
 class WelcomeMacro(WikiMacroBase):
 
     def expand_macro(self, formatter, name, args):
-        """ Return a welcome heading with the project name extracted from trac.ini.
+        """Return a welcome heading with the project name extracted from trac.ini.
 
-            + User-defined welcome string suffix and prefix.
-            + Supports both standard and dictionary methods.
-            + With defaults.
+           + User-defined welcome string suffix and prefix.
+           + Supports both standard and dictionary methods.
+           + With defaults.
         """
         prefix = suffix = ''
         args, kw = parse_args(args)
@@ -45,15 +44,5 @@ class WelcomeMacro(WikiMacroBase):
         if not prefix: prefix = default['prefix']
         if not suffix: suffix = default['suffix']
 
-        section = 'project'
-        option = 'name'
-
-        if self.config.has_option(section, option):
-            return tag.h1('%s %s %s' % (prefix,
-                                        self.config.get(section, option),
-                                        suffix),
-                          id='welcome')
-        else:
-            return system_message('%s: No option "%s" in section [%s]' % (name,
-                                                                          option,
-                                                                          section))
+        return tag.h1('%s %s %s' % (prefix, self.env.project_name, suffix),
+                      id='welcome')
